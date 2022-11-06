610edeacc49beaf1b54b53d52e61883bc222746f
Richard Ramirez <hackysmack69@gmail.com>
2022-11-06 16:35:32 -0600

    changes from tutorial 2 db changes

--- log_ger/main/views.py
+++ log_ger/main/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import ToDoList,Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Welcome to THE LOG_ger Home Page!</h1>")
-def v1(response):
-    return HttpResponse("<h1>Welcome to THE LOG_ger V1 Page!</h1>")
-
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    lsitem = ls.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1><hr><h1>%s</h1>" %(ls.name, str(lsitem.text)))
