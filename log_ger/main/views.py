d48f1eb3964320b893fdeb3c72ef0609e7afaf53
Richard Ramirez <hackysmack69@gmail.com>
2022-11-06 18:01:23 -0600

    updates from video 3

--- log_ger/main/views.py
+++ log_ger/main/views.py
@@ -6,5 +6,8 @@ from .models import ToDoList,Item
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    lsitem = ls.item_set.get(id=1)
-    return HttpResponse("<h1>%s</h1><hr><h1>%s</h1>" %(ls.name, str(lsitem.text)))
+    return render(response, "main/base.html", {})
+
+def home(response):
+    return render(response, "main/home.html", {})
+    
